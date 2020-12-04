0a451031869de7924a372e75f5e24b9268dea619
E-m-o <vineet.biscuit@gmail.com>
2020-12-04 15:08:06 +0530

    modify date time file

--- cv2.show_date_time_on_vids.py
+++ cv2.show_date_time_on_vids.py
@@ -10,8 +10,8 @@ while(capture.isOpened()):
         
         font = cv2.FONT_HERSHEY_SIMPLEX
         text = 'Width: ' + str(capture.get(3)) + 'Height: ' + str(capture.get(4))
-        datet = str(datetime.datetime.now())
-        frame = cv2.putText(frame, datet, (10,50), font, 1, (0,0,255), 2, cv2.LINE_AA)
+        date_now = str(datetime.datetime.now())
+        frame = cv2.putText(frame, date_now, (10,50), font, 1, (0,0,255), 2, cv2.LINE_AA)
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
